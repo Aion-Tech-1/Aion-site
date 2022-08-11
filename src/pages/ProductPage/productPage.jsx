21b64a897d62ddc6a5ae46b0fbb1da0061d3279f
Raghav Chandra V S <raghavchandravs@gmail.com>
2022-08-11 23:21:45 +0530

    style: section-1

--- src/pages/ProductPage/productPage.jsx
+++ src/pages/ProductPage/productPage.jsx
@@ -2,11 +2,9 @@ import "./productPage.css";
 
 export const ProductPage = () => {
   return (
-    <div>
+    <div className="product-page">
       <section className="product-overview">
-        <div className="prod-img-container">
-          <img src="images/PROD-IMG1.PNG" className="prod-img" alt="" srcset="" />
-        </div>
+
 
         <div className="prod-info">
           <div className="overview">PRODUCT OVERVIEW</div>
@@ -16,7 +14,7 @@ export const ProductPage = () => {
           </div>
 
           <p className="prod-desc">
-            The Abilitech™ Assist — a one-of-a-kind wearable assistive device
+            The AION Assist — a one-of-a-kind wearable assistive device
             that supports patients with upper-limb weakness or injury in
             completing essential activities of daily living.
           </p>
@@ -26,6 +24,26 @@ export const ProductPage = () => {
             <button className="prod-btn">PRODUCT DEMO</button>
           </div>
         </div>
+
+        <div className="prod-img-container">
+          <img src="images/PROD-IMG1.PNG" className="prod-img" alt="" srcset="" />
+        </div>
+      </section>
+
+
+      <section className="prod-section">
+
+<div className="prod-img2"> <img src="" className="prod-img2" alt="" srcset="" /></div>
+
+<div className="section-info">
+
+
+</div>
+      </section>
+
+
+      <section className="prod-section2">
+
       </section>
     </div>
   );
